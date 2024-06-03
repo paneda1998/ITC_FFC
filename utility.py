a4d9d4cc1a7e339669640709b37a5411f3935aa5
Kiarash <kiarash.geraili@gmail.com>
2024-06-03 22:16:29 +0330

    Update utility.py
    Features:
    
    there are two new functions one for data manipulating (consisting of shuffling, feature or class sub data creating, different data saving etc.)
    
    the second function is for NPZ data reading in a correct way (adding column names and etc.)

--- utility.py
+++ utility.py
@@ -40,3 +40,77 @@ def csv_to_npz(csv_file_path, npz_save_path):
     
     # Save the NumPy array to an NPZ file
     np.savez(npz_save_path, data=data)
+
+def process_dataset(csv_file, shuffle = False, select_features = None, select_classes = None, output_format = 'npz'):
+    
+    # Load the CSV file into a DataFrame
+    df = pd.read_csv(csv_file)
+    
+    # Select only specified features and classes
+    if select_features:
+        select_features.append(df.columns[-1])
+        df = df[select_features]
+    
+    if select_classes:
+        df = df[df[df.columns[-1]].isin(select_classes)]
+    
+    # Shuffle the dataset if specified
+    #oke
+    if shuffle:
+        df = df.sample(frac=1).reset_index(drop=True)
+    
+    # Convert the DataFrame to the desired format    
+    data = df.to_numpy()
+    # Saving column names for further creating files
+    columns_name = df.columns.to_numpy()
+    
+    if output_format == 'npz':
+        
+        # Reshape arr1 to (1, len(features)) so it can be stacked as a row
+        columns_name_reshaped = columns_name.reshape(1, len(columns_name))
+    
+        # Stack arr1_reshaped on top of arr2
+        data = np.vstack((columns_name_reshaped, data))
+        
+        # Now result is a (len(data)+1, len(features)) array with arr1 as the first row
+        np.savez('output.npz', data=data)
+        
+        print("Data saved as output.npz")
+        
+    elif output_format == 'mat':
+        savemat('output.mat', {'data': data})
+        print("Data saved as output.mat")
+        
+    elif output_format == 'dat':
+        data.tofile('output.dat')
+        print("Data saved as output.dat")
+        
+    else:
+        raise ValueError("Unsupported output format. Choose from 'NPZ', 'MAT', or 'DAT'.")
+        
+def read_npz(npz_path):
+    
+    #this function read a npz file and create a dataframe based on the npz file
+    #we assumed that npz file consists of a column array following the data like this:
+    
+    #[['BFD_0' 'BFD_1' 'BFD_2' ... 'MP3_Sync' 'FLAC_Sync' 'Class Label (1']
+    #[2.625 2.375 1.5625 ... 0.62521 1.0004 0.0]
+    #[2.0625 1.8125 1.125 ... 12.629 6.5026 0.0]
+    #...
+    #[7.1875 2.0625 2.125 ... 0.37513 0.5002 2.0]
+    #[1.5 1.6875 1.4375 ... 0.75025 0.5002 2.0]
+    #[1.3125 2.25 1.75 ... 1.3755 1.5006 2.0]]
+    
+    loaded_data = np.load('output.npz', allow_pickle = True)
+
+    arr_loaded = loaded_data['data']
+
+    df = pd.DataFrame(arr_loaded)
+
+    # Convert the loaded numpy array to a pandas DataFrame
+    df.rename(columns = df.iloc[0], inplace = True)
+
+    #dropping column names
+    df.drop(df.index[0], inplace = True)
+
+    return df
